88316e98735910db3a0cefe5754a7782018cf448
SkyFly <artem.burahin78@gmail.com>
2019-10-08 20:08:24 +0300

    Конверт

---
+++ src/main/java/Convert.java
@@ -0,0 +1,22 @@
+public class Convert {
+    public Convert(float f1, float f2) {
+        faceit1 = f1;
+        faceit2 = f2;
+    }
+
+    public Convert(){
+
+    }
+
+    private float faceit1;
+    private float faceit2;
+
+    public float getFaceit1() {
+        return faceit1;
+    }
+
+    public float getFaceit2() {
+        return faceit1;
+    }
+}
+
